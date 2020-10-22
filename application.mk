ad53eab69c916429ba94353fecd6387dd08b5c5b
Joshua Wong <joshua@northernmechatronics.com>
2020-10-22 15:40:08 -0400

    Added comment.

--- application.mk
+++ application.mk
@@ -1,3 +1,8 @@
+#******************************************************************************
+#
+# Define the locations of the various SDKs and libraries.
+#
+#******************************************************************************
 NM_SDK    := $(HOME)/git/nmsdk
 AMBIQ_SDK := $(HOME)/git/AmbiqSuite-R2.5.1
 FREERTOS  := $(HOME)/git/FreeRTOS/FreeRTOS
@@ -5,4 +10,9 @@ CORDIO    := $(HOME)/git/AmbiqSuite-R2.5.1/third_party/cordio
 UECC      := $(HOME)/git/AmbiqSuite-R2.5.1/third_party/uecc
 LORAMAC   := $(HOME)/git/loramac-node
 
+#******************************************************************************
+#
+# Specify the location of the board support package to be used.
+#
+#******************************************************************************
 BSP_DIR := $(NM_SDK)/bsp/nm180100evb
